aa0c2f2ea32d215df52dcd62a063be3042b14497
David Mulder <dmulder@suse.com>
2017-09-21 05:38:50 -0600

    Fetch the list of users, groups and computers

--- src/complex.py
+++ src/complex.py
@@ -6,4 +6,47 @@ from samba.net import Net
 from samba.dcerpc import nbt
 import uuid
 import re
+from subprocess import Popen, PIPE
+from syslog import syslog, LOG_INFO, LOG_ERR, LOG_DEBUG, LOG_EMERG, LOG_ALERT
+
+class Connection:
+    def __init__(self, lp, creds):
+        self.lp = lp
+        self.creds = creds
+        self.realm = lp.get('realm')
+        net = Net(creds=creds, lp=lp)
+        cldap_ret = net.finddc(domain=self.realm, flags=(nbt.NBT_SERVER_LDAP | nbt.NBT_SERVER_DS))
+        self.l = ldap.initialize('ldap://%s' % cldap_ret.pdc_dns_name)
+        if self.__kinit_for_gssapi():
+            auth_tokens = ldap.sasl.gssapi('')
+            self.l.sasl_interactive_bind_s('', auth_tokens)
+        else:
+            self.l.bind_s('%s@%s' % (creds.get_username(), self.realm) if not self.realm in creds.get_username() else creds.get_username(), creds.get_password())
+
+    def __kinit_for_gssapi(self):
+        p = Popen(['kinit', '%s@%s' % (self.creds.get_username(), self.realm) if not self.realm in self.creds.get_username() else self.creds.get_username()], stdin=PIPE, stdout=PIPE)
+        p.stdin.write('%s\n' % self.creds.get_password())
+        return p.wait() == 0
+
+    def realm_to_dn(self, realm):
+        return ','.join(['DC=%s' % part for part in realm.lower().split('.')])
+
+    def __well_known_container(self, container):
+        if container == 'system':
+            wkguiduc = 'AB1D30F3768811D1ADED00C04FD8D5CD'
+        elif container == 'computers':
+            wkguiduc = 'AA312825768811D1ADED00C04FD8D5CD'
+        elif container == 'dcs':
+            wkguiduc = 'A361B2FFFFD211D1AA4B00C04FD7D83A'
+        elif container == 'users':
+            wkguiduc = 'A9D1CA15768811D1ADED00C04FD8D5CD'
+        result = self.l.search_s('<WKGUID=%s,%s>' % (wkguiduc, self.realm_to_dn(self.realm)), ldap.SCOPE_SUBTREE, '(objectClass=container)', ['distinguishedName'])
+        if result and len(result) > 0 and len(result[0]) > 1 and 'distinguishedName' in result[0][1] and len(result[0][1]['distinguishedName']) > 0:
+            return result[0][1]['distinguishedName'][-1]
+
+    def user_group_list(self):
+        return self.l.search_s(self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=person)', []) + self.l.search_s(self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=group)', [])
+
+    def computer_list(self):
+        return self.l.search_s(self.__well_known_container('computers'), ldap.SCOPE_SUBTREE, '(objectCategory=computer)', [])
 
--- src/dialogs.py
+++ src/dialogs.py
@@ -10,34 +10,53 @@ from ycp import *
 ycp.widget_names()
 import Wizard
 
-from yui import Empty, HWeight, ReplacePoint, HBox
+from complex import Connection
+from yui import Empty, HWeight, ReplacePoint, HBox, Tree, Node
+
+from syslog import syslog, LOG_INFO, LOG_ERR, LOG_DEBUG, LOG_EMERG, LOG_ALERT
 
 class ADUC:
     def __init__(self, lp, creds):
         self.realm = lp.get('realm')
         self.lp = lp
         self.creds = creds
+        try:
+            self.conn = Connection(lp, creds)
+            self.users = self.conn.user_group_list()
+            self.computers = self.conn.computer_list()
+        except Exception as e:
+          syslog(LOG_EMERG, str(e))
 
     def Show(self):
         Wizard.SetContentsButtons(gettext.gettext('Active Directory Users and Computers'), self.__aduc_page(), self.__help(), 'Back', 'Next')
         Wizard.DisableBackButton()
         Wizard.DisableNextButton()
-        #UI.SetFocus(Term('id', 'aduc_tree'))
+        UI.SetFocus(Term('id', 'aduc_tree'))
 
         ret = Symbol('abort')
         while True:
             ret = UI.UserInput()
             if str(ret) == 'abort' or str(ret) == 'cancel':
                 break
+            elif str(ret) == 'aduc_tree':
+                choice = UI.QueryWidget(Term('id', 'aduc_tree'), Symbol('Value'))
 
         return ret
 
     def __help(self):
         return ''
 
+    def __aduc_tree(self):
+        return Tree('Active Directory Users and Computers', [
+            Node('my.domain', True, [
+                Node('Users', True),
+                Node('Computers', True),
+            ]),
+        ], id='aduc_tree', opts=['notify'])
+
     def __aduc_page(self):
         return HBox([
-            HWeight(1, Empty()),
-            HWeight(2, ReplacePoint(Empty(), id='rightPane'))]
-            )
+            HWeight(1, self.__aduc_tree()),
+            HWeight(2, ReplacePoint(Empty(), id='rightPane'))
+        ])
 
