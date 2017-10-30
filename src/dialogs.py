b4cf8a10f09bdfe90efa0c85ddb3b5ff59fc72be
David Mulder <dmulder@suse.com>
2017-10-30 02:11:58 -0600

    Add MinSize to dialogs, so data isn't squashed

--- src/dialogs.py
+++ src/dialogs.py
@@ -36,19 +36,19 @@ UserDataModel = {
 
 UserTabContents = {
         'address' : {
-            'content' : (lambda model: VBox(HBox(
+            'content' : (lambda model: MinSize(50, 30, VBox(HBox(
                 Label(Id('street'), UserDataModel['address']['streetAddress']),
                 RichText(Id('streetAddress'), model.get_value('streetAddress'))),
                 Left(InputField(Id('postOfficeBox'), Opt('hstretch'), UserDataModel['address']['postOfficeBox'], model.get_value('postOfficeBox'))),
                 Left(InputField(Id('l'), Opt('hstretch'), UserDataModel['address']['l'], model.get_value('l'))),
                 Left(InputField(Id('st'), Opt('hstretch'), UserDataModel['address']['st'], model.get_value('st'))),
                 Left(InputField(Id('postalCode'), Opt('hstretch'), UserDataModel['address']['postalCode'], model.get_value('postalCode',))),
-                Left(InputField(Id('co'), Opt('hstretch'), UserDataModel['address']['co'], model.get_value('co'))))),
+                Left(InputField(Id('co'), Opt('hstretch'), UserDataModel['address']['co'], model.get_value('co')))))),
             'data' : UserDataModel['address'],
             'title' : 'Address'
             },
         'general' : {
-            'content' : (lambda model: VBox( Left(HBox(
+            'content' : (lambda model: MinSize(50, 30, VBox(Left(HBox(
                 InputField(Id('givenName'), Opt('hstretch'), UserDataModel['general']['givenName'], model.get_value('givenName')),
                 InputField(Id('initials'), Opt('hstretch'), UserDataModel['general']['initials'], model.get_value('initials')))),
                 Left(InputField(Id('sn'), Opt('hstretch'), UserDataModel['general']['sn'], model.get_value('sn'))),
@@ -57,7 +57,7 @@ UserTabContents = {
                 Left(InputField(Id('telephoneNumber'), Opt('hstretch'), UserDataModel['general']['telephoneNumber'], model.get_value('telephoneNumber'))),
                 Left(InputField(Id('mail'), Opt('hstretch'), UserDataModel['general']['mail'], model.get_value('mail'))),
                 Left(InputField(Id('wWWHomePage'), Opt('hstretch'), UserDataModel['general']['wWWHomePage'], model.get_value('wWWHomePage')))
-            )),
+            ))),
             'data' : UserDataModel['general'],
             'title' : 'General'
             }
@@ -201,28 +201,28 @@ ComputerDataModel = {
 
 ComputerTabContents = {
         'general' : {
-            'content' : (lambda model: VBox(
+            'content' : (lambda model: MinSize(50, 30, VBox(
                 InputField(Id('name'), Opt('disabled', 'hstretch'), ComputerDataModel['general']['name'], model.get_value('name')),
                 InputField(Id('dNSHostName'), Opt('disabled', 'hstretch'), ComputerDataModel['general']['name'], model.get_value('dNSHostName')),
                 # #TODO find out what attribute site is
                 InputField(Id('idontknow'), Opt('disabled', 'hstretch'), ComputerDataModel['general']['idontknow'], "Workstation or server"),
-                InputField(Id('description'), Opt('hstretch'), ComputerDataModel['general']['description'], model.get_value('description')))),
+                InputField(Id('description'), Opt('hstretch'), ComputerDataModel['general']['description'], model.get_value('description'))))),
 
             'data' : ComputerDataModel['general'],
             'title': 'General'
             },
 
         'operating_system' : {
-            'content' : (lambda model: VBox(
+            'content' : (lambda model: MinSize(50, 30, VBox(
                   InputField(Id('operatingSystem'), Opt('disabled', 'hstretch'), ComputerDataModel['operating_system']['operatingSystem'], model.get_value('operatingSystem')),
                   InputField(Id('operatingSystemVersion'), Opt('disabled', 'hstretch'),ComputerDataModel['operating_system']['operatingSystemVersion'], model.get_value('operatingSystemVersion')),
-                  InputField(Id('operatingSystemServicePack'), Opt('disabled', 'hstretch'), ComputerDataModel['operating_system']['operatingSystemServicePack'], model.get_value('operatingSystemServicePack')))),
+                  InputField(Id('operatingSystemServicePack'), Opt('disabled', 'hstretch'), ComputerDataModel['operating_system']['operatingSystemServicePack'], model.get_value('operatingSystemServicePack'))))),
             'data' : ComputerDataModel['operating_system'],
             'title': 'Operating System'
             },
         'location' : {
-            'content' : (lambda model: VBox(
-                TextEntry(Id('location'), Opt('hstretch'), ComputerDataModel['location']['location'], model.get_value('location')))),
+            'content' : (lambda model: MinSize(50, 30, VBox(
+                TextEntry(Id('location'), Opt('hstretch'), ComputerDataModel['location']['location'], model.get_value('location'))))),
             'data' : ComputerDataModel['location'],
             'title': 'Location'
             }
