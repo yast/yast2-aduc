f20d9c592b5268606a23268f7811e92d2533ebd6
David Mulder <dmulder@suse.com>
2018-05-31 10:36:11 -0600

    Handle ldap calls better

--- src/complex.py
+++ src/complex.py
@@ -11,6 +11,91 @@ from subprocess import Popen, PIPE
 from syslog import syslog, LOG_INFO, LOG_ERR, LOG_DEBUG, LOG_EMERG, LOG_ALERT
 from ldap.modlist import addModlist as addlist
 from ldap.modlist import modifyModlist as modlist
+import traceback
+
+PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2
+
+class LdapException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+        if len(self.args) > 0:
+            self.msg = self.args[0]
+        else:
+            self.msg = None
+        if len(self.args) > 1:
+            self.info = self.args[1]
+        else:
+            self.info = None
+
+def _ldap_exc_msg(e):
+    if len(e.args) > 0 and \
+      type(e.args[-1]) is dict and \
+      'desc' in e.args[-1]:
+        return e.args[-1]['desc']
+    else:
+        return str(e)
+
+def _ldap_exc_info(e):
+    if len(e.args) > 0 and \
+      type(e.args[-1]) is dict and \
+      'info' in e.args[-1]:
+        return e.args[-1]['info']
+    else:
+        return ''
+
+def ldap_search(l, *args):
+    try:
+        return l.search_s(*args)
+    except Exception as e:
+        traceback.print_exc(file=sys.stderr)
+        sys.stderr.write('ldap.search_s: %s\n' % _ldap_exc_msg(e))
+
+def ldap_add(l, *args):
+    try:
+        return l.add_s(*args)
+    except Exception as e:
+        raise LdapException(_ldap_exc_msg(e), _ldap_exc_info(e))
+
+def ldap_modify(l, *args):
+    try:
+        return l.modify(*args)
+    except Exception as e:
+        traceback.print_exc(file=sys.stderr)
+        sys.stderr.write('ldap.modify: %s\n' % _ldap_exc_msg(e))
+
+def ldap_delete(l, *args):
+    try:
+        return l.delete_s(*args)
+    except Exception as e:
+        traceback.print_exc(file=sys.stderr)
+        sys.stderr.write('ldap.delete_s: %s\n' % _ldap_exc_msg(e))
+
+def stringify_ldap(data):
+    if type(data) == dict:
+        for key, value in data.items():
+            data[key] = stringify_ldap(value)
+        return data
+    elif type(data) == list:
+        new_list = []
+        for item in data:
+            new_list.append(stringify_ldap(item))
+        return new_list
+    elif type(data) == tuple:
+        new_tuple = []
+        for item in data:
+            new_tuple.append(stringify_ldap(item))
+        return tuple(new_tuple)
+    elif PY2 and type(data) == unicode:
+        return str(data)
+    elif PY3 and type(data) == bytes:
+        try:
+            return data.decode('utf-8')
+        except UnicodeDecodeError:
+            return data
+    else:
+        return data
+
 class Connection:
     def __init__(self, lp, creds):
         self.lp = lp
@@ -42,15 +127,15 @@ class Connection:
             wkguiduc = 'A361B2FFFFD211D1AA4B00C04FD7D83A'
         elif container == 'users':
             wkguiduc = 'A9D1CA15768811D1ADED00C04FD8D5CD'
-        result = self.l.search_s('<WKGUID=%s,%s>' % (wkguiduc, self.realm_to_dn(self.realm)), ldap.SCOPE_SUBTREE, '(objectClass=container)', ['distinguishedName'])
+        result = ldap_search(self.l, '<WKGUID=%s,%s>' % (wkguiduc, self.realm_to_dn(self.realm)), ldap.SCOPE_SUBTREE, '(objectClass=container)', stringify_ldap(['distinguishedName']))
         if result and len(result) > 0 and len(result[0]) > 1 and 'distinguishedName' in result[0][1] and len(result[0][1]['distinguishedName']) > 0:
             return result[0][1]['distinguishedName'][-1]
 
     def user_group_list(self):
-        return self.l.search_s(self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=person)', []) + self.l.search_s(self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=group)', [])
+        return ldap_search(self.l, self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=person)', []) + ldap_search(self.l, self.__well_known_container('users'), ldap.SCOPE_SUBTREE, '(objectCategory=group)', [])
 
     def computer_list(self):
-        return self.l.search_s(self.__well_known_container('computers'), ldap.SCOPE_SUBTREE, '(objectCategory=computer)', [])
+        return ldap_search(self.l, self.__well_known_container('computers'), ldap.SCOPE_SUBTREE, '(objectCategory=computer)', [])
 
     def update(self, dn, orig_map, modattr, addattr):
         try:
@@ -59,11 +144,11 @@ class Connection:
                 for key in modattr:
                     oldattr[key] = orig_map.get(key, [])
                 print '##### attempting mod %s'%modattr
-                self.l.modify_s(dn, modlist(oldattr, modattr))
+                ldap_modify(self.l, dn, stringify_ldap(modlist(oldattr, modattr)))
                 print '##### appeared to work mod %s with %s'%(dn,modattr)
             if len(addattr):
                 print '##### attempting add %s'%addattr
-                self.l.add_s(dn, addlist(addattr))
+                ldap_add(self.l, dn, addlist(stringify_ldap(addattr)))
                 print '##### appeared to work add %s with %s'%(dn,addattr)
         except Exception as e:
             print '##### exception %s'%e
