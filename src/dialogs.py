d15c7c88f39b94285a53c47f9560720339a6607f
David Mulder <dmulder@suse.com>
2018-12-20 06:57:48 -0700

    Use new yast2-adcommon-python code

--- src/dialogs.py
+++ src/dialogs.py
@@ -2,7 +2,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import copy
-from complex import Connection, strcmp, validate_kinit
+from complex import Connection
+from adcommon.strings import strcmp
 from random import randint
 from yast import import_module
 import_module('Wizard')
@@ -10,8 +11,8 @@ import_module('UI')
 from yast import *
 import six
 from ldap.filter import filter_format
-from ldap import SCOPE_SUBTREE as SUBTREE
-from samba.credentials import MUST_USE_KERBEROS
+from adcommon.yldap import SCOPE_SUBTREE as SUBTREE
+from adcommon.creds import YCreds
 
 def have_x():
     from subprocess import Popen, PIPE
@@ -908,16 +909,15 @@ class ADUC:
         self.realm = lp.get('realm')
         self.lp = lp
         self.creds = creds
-        self.got_creds = self.__get_creds(creds)
+        ycred = YCreds(creds)
+        self.got_creds = ycred.get_creds()
         while self.got_creds:
             try:
                 self.conn = Connection(lp, creds)
                 break
             except Exception as e:
                 ycpbuiltins.y2error(str(e))
-                creds.set_password('')
-                self.got_creds = self.__get_creds(creds)
-
+                self.got_creds = ycred.get_creds()
 
     def __delete_selected_obj(self, container):
         currentItemName = UI.QueryWidget('items', 'CurrentItem')
@@ -925,50 +925,6 @@ class ADUC:
         currentItem = self.__find_by_name(searchList, currentItemName)
         if self.__warn_delete(currentItem[-1]['name'][-1]):
             self.conn.ldap_delete(currentItem[0])
-
-    def __get_creds(self, creds):
-        if not creds.get_password():
-            if creds.get_username():
-                validate_kinit(self.creds)
-                if self.creds.get_kerberos_state() == MUST_USE_KERBEROS:
-                    return True
-            UI.SetApplicationTitle('Authenticate')
-            UI.OpenDialog(self.__password_prompt(creds.get_username()))
-            while True:
-                subret = UI.UserInput()
-                if str(subret) == 'creds_ok':
-                    user = UI.QueryWidget('username_prompt', 'Value')
-                    password = UI.QueryWidget('password_prompt', 'Value')
-                    UI.CloseDialog()
-                    if not password:
-                        return False
-                    creds.set_username(user)
-                    creds.set_password(password)
-                    return True
-                if str(subret) == 'creds_cancel':
-                    UI.CloseDialog()
-                    return False
-                if str(subret) == 'username_prompt':
-                    user = UI.QueryWidget('username_prompt', 'Value')
-                    creds.set_username(user)
-                    validate_kinit(self.creds)
-                    if self.creds.get_kerberos_state() == MUST_USE_KERBEROS:
-                        UI.CloseDialog()
-                        return True
-        return True
-
-    def __password_prompt(self, user):
-        return MinWidth(30, HBox(HSpacing(1), VBox(
-            VSpacing(.5),
-            Left(Label('To continue, type an administrator password')),
-            Left(TextEntry(Id('username_prompt'), Opt('hstretch', 'notify'), 'Username', user)),
-            Left(Password(Id('password_prompt'), Opt('hstretch'), 'Password')),
-            Right(HBox(
-                PushButton(Id('creds_ok'), 'OK'),
-                PushButton(Id('creds_cancel'), 'Cancel'),
-            )),
-            VSpacing(.5)
-        ), HSpacing(1)))
 
     def __show_properties(self, container):
         searchList = []
